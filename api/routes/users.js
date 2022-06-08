a96b309efa0ecf14b434918488be34f54176841c
Pavel Strutinsky <pvorobiev@mmtr.ru>
2022-06-08 16:40:07 +0300

    init booling app

---
+++ api/routes/users.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+
+const router = express.Router();
+
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/", getUsers);
+
+export default router
