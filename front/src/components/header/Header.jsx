fe7beba94b0a0257ea031126d561ab4fab931d63
Paul Strutinksy <voidstein@gmail.com>
2022-06-10 03:37:04 +0300

    dev header

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,13 +1,36 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBed, faPlane, faCar, faTaxi } from '@fortawesome/free-solid-svg-icons';
 import './header.css';
 
 export const Header = () => {
   return (
     <div className="header">
-        <div className="headerList">
-            <div className="headerListItem">
-            {/* <FontAwesomeIcon icon={"fa-solid fa-bed"} /> */}
+        <div className="headerContainer">
+            <div className="headerList">
+                <div className="headerListItem active">
+                    <FontAwesomeIcon icon={faBed} />
+                    <span>Stays</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faPlane} />
+                    <span>Flight</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faCar} />
+                    <span>Car Rentals</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faBed} />
+                    <span>Attarctions</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faTaxi} />
+                <span>Airport taxis</span>
+                </div>
             </div>
+            <h1 className="headerTitle">A lifetime of discounts? It's genius.</h1>
+            <p className="headerDesc">Get rewarded for your travels - unlock instant savings of 10% or mo with a free Exminbooking account.</p>
+            <button className="headerBtn">Sign in / Register</button>
         </div>
     </div>
   )
