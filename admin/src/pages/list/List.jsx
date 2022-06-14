e47c0f6ad7ad2274da14fcf4b26e3468ae7076da
Pavel Strutinsky <pvorobiev@mmtr.ru>
2022-06-14 17:37:46 +0300

    dev lists

--- admin/src/pages/list/List.jsx
+++ admin/src/pages/list/List.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import "./list.scss";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Datatable from "../../components/datable/Datatable";
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
 };
 
 export default List;
