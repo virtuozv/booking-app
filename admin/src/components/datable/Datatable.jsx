18e4ce8a9bb9bc71072db79a4570ea69465d8a0a
Pavel Strutinsky <pvorobiev@mmtr.ru>
2022-06-15 16:02:29 +0300

    fixed links products/main/users/newUsers/:idUsers

--- admin/src/components/datable/Datatable.jsx
+++ admin/src/components/datable/Datatable.jsx
@@ -1,5 +1,6 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
 import { userColumns, userRows } from "../../datatablesource";
 
 const Datatable = () => {
@@ -11,7 +12,9 @@ const Datatable = () => {
       renderCell: () => {
         return (
           <div className="cellAction">
-            <div className="viewButton">View</div>
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
             <div className="deleteButton">Delete</div>
           </div>
         );
@@ -21,6 +24,16 @@ const Datatable = () => {
 
   return (
     <div className="datatable">
+      <div className="datatableTitle">
+        Add New User
+        <Link
+          to="/users/new"
+          style={{ textDecoration: "none" }}
+          className="link"
+        >
+          Add New
+        </Link>
+      </div>
       <DataGrid
         rows={userRows}
         columns={userColumns.concat(actionColumn)}
--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -17,7 +17,9 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <span className="logo">admin</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">admin</span>
+        </Link>
       </div>
       <hr />
       <div className="center">
@@ -28,14 +30,18 @@ const Sidebar = () => {
             <Link to="/">Dashboard</Link>
           </li>
           <p className="title">LIST</p>
-          <li>
-            <PersonOutlineOutlinedIcon className="icon" />
-            <Link to="/users">Users</Link>
-          </li>
-          <li>
-            <AllInboxIcon className="icon" />
-            <span>Products</span>
-          </li>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineOutlinedIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <AllInboxIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
           <li>
             <AndroidIcon className="icon" />
             <span>Orders</span>
